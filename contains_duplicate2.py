d75849c3608541a69d26f7ceddef06d194e0cbf8
jied <jied314@mail.sfsu.edu>
2015-06-15 12:45:37 -0700

    HashTable - Contain Duplicate

---
+++ contains_duplicate2.py
@@ -0,0 +1,27 @@
+# Array, HashTable - Contain Duplicate2
+# Given an array of integers and an integer k, find out whether there
+# are two distinct indices i and j in the array such that nums[i] = nums[j]
+# and the difference between i and j is at most k.
+#
+class ContainDuplicate2:
+    # @param {integer[]} nums
+    # @param {integer} k
+    # @return {boolean}
+    # test on LeetCode - 56ms
+    def containsNearbyDuplicate(self, nums, k):
+    	visited = {}
+    	for i in range(0, len(nums)):
+    		num = nums[i]
+    		if visited.has_key(num):
+    			if i - visited[num] <= k:
+    				return True
+    			visited[num] = i
+    		visited[num] = i
+    	return False
+
+def main():
+	test = ContainDuplicate2()
+	print test.containsNearbyDuplicate([1, 2, 3, 1, 1], 1)
+
+if __name__ == '__main__':
+	main()
