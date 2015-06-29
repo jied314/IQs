e8e6777368e5dd8c59fd689aaed008e20504db4a
jied <jied314@mail.sfsu.edu>
2015-06-28 17:31:15 -0700

    use bit masks

--- Medium/SingleNumber2.java
+++ Medium/SingleNumber2.java
@@ -21,11 +21,12 @@ public class SingleNumber2 {
     	return num;
     }
 
-    public int singleNumberError(int[] A) {
+    public int singleNumber_first(int[] A) {
     	int[] result = new int[32];
     	int num = 0;
     	for (int i = 0; i < 32; i++) {
     		for (int j = 0; j < A.length; j++) {
+                // wrong - result[i] += A[j] & (1 << i); 
     			result[i] += (A[j] & (1 << i)) == 0 ? 0: 1;
     		}
     		result[i] %= 3;
@@ -33,6 +34,26 @@ public class SingleNumber2 {
     	}
     	// System.out.println(Arrays.toString(result));
     	return num;
+    }
+
+    /**
+     * use bitmask variables
+     * 1. ones as a bitmask to represent the ith bit had appeared once.
+     * 2. twos as a bitmask to represent the ith bit had appeared twice.
+     * 3. threes as a bitmask to represent the ith bit had appeared three times.
+     * When the ith bit had appeared for the third time, clear the ith bit of both ones and twos to 0. 
+     * The final answer will be the value of ones.
+     */
+    public int singleNumber(int A[], int n) {
+        int ones = 0, twos = 0, threes = 0;
+        for (int i = 0; i < n; i++) {
+            twos |= ones & A[i]; // find the common bit patterns (appear twice)
+            ones ^= A[i];
+            threes = ones & twos;
+            ones &= ~threes;
+            twos &= ~threes;
+        }
+        return ones;
     }
 
     public static void main(String[] args) {
