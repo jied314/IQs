2ebc5c68bf2435219936d11b27e2a4bed0eccb3f
jied <jied314@mail.sfsu.edu>
2015-07-05 21:38:47 -0700

    Linked List - Swap Nodes

---
+++ Medium/swap_nodes.py
@@ -0,0 +1,59 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+# 7/5 - Linked List
+# Given a linked list, swap every two adjacent nodes and return its head.
+class SwapPairs:
+    # @param {ListNode} head
+    # @return {ListNode}
+
+    # Error - forget update previous to latter
+    def swap_pairs_error(self, head):
+        if head and head.next:
+            previous = head
+            head = head.next
+            while previous is not None and previous.next:
+                cur = previous.next
+                previous.next = cur.next
+                cur.next = previous
+                previous = previous.next
+        self.traverse(head)
+        return head
+
+    def swap_pairs_nice(self, head):
+        pre = ListNode(0)
+        pre.next = head
+        cur = head
+        head = pre
+        while cur and cur.next:
+            pre.next = cur.next
+            cur.next = pre.next.next
+            pre.next.next = cur
+            pre = cur
+            cur = cur.next
+        return head.next
+
+    def traverse(self, node):
+        while node is not None:
+            print node.val
+            node = node.next
+
+
+def main():
+    test = SwapPairs()
+    head = ListNode(2)
+    one = ListNode(1)
+    three = ListNode(3)
+    four = ListNode(4)
+    head.next = one
+    one.next = three
+    three.next = four
+    print test.swap_pairs(head)
+
+
+if __name__ == '__main__':
+    main()
