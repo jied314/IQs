d562d2e3da71a98b0aea681ddf4e9f7de8cc94a2
jied <jied314@mail.sfsu.edu>
2015-08-17 20:11:15 -0700

    BackTracking - combination sum

---
+++ Medium/combination_sum2.py
@@ -0,0 +1,67 @@
+# 8/17 - Array, BackTracking
+# Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where
+# the candidate numbers sums to T.
+# Each number in C may only be used once in the combination.
+# Note:
+#   All numbers (including target) will be positive integers.
+#   Elements in a combination (a1, a2, ..., ak) must be in non-descending order. (ie, a1 <= a2 <= ... <= ak).
+#   The solution set must not contain duplicate combinations.
+#   For example, given candidate set 10,1,2,7,6,1,5 and target 8,
+#   A solution set is: [1, 7], [1, 2, 5], [2, 6], [1, 1, 6]
+#
+
+import copy
+
+class CombinationSum2:
+    result = []  # static variable to store result
+
+    # Test on LeetCode - 116ms
+    # use less copy.deepcopy
+    # @param {integer[]} candidates
+    # @param {integer} target
+    # @return {integer[][]}
+    def combination_sum_recursive(self, candidates, target):
+        CombinationSum2.result = []
+        candidates.sort()
+        length = len(candidates)
+        self.visit([], target, 0, candidates, length)
+        return CombinationSum2.result
+
+    def visit(self, base, left, index, candidates, length):
+        if left == 0:
+            CombinationSum2.result.append(copy.deepcopy(base))  # need deepcopy
+        else:
+            if self.is_promising(left, index, candidates, length):
+                j = 1
+                num = candidates[index]
+                while index < length-1 and candidates[index] == candidates[index+1]:
+                    index += 1
+                    j += 1
+                index += 1
+                if j > 1:  # contains duplicates
+                    j = min(j, left/num)
+                    for i in range(j, 0, -1):
+                        base.append(num)
+                        left -= num
+                    for i in range(0, j):
+                        self.visit(base, left, index, candidates, length)
+                        base.pop()
+                        left += num
+                    self.visit(base, left, index, candidates, length)
+                else:  # no duplicates
+                    base.append(num)
+                    self.visit(base, left-num, index, candidates, length)
+                    base.pop()
+                    self.visit(base, left, index, candidates, length)
+
+    def is_promising(self, left, index, candidates, length):
+        return index < length and left >= candidates[index]
+
+def main():
+    test = CombinationSum2()
+    # print test.combination_sum_recursive([10,1,2,7,6,1,5], 8)
+    print test.combination_sum_recursive([1], 2)
+
+
+if __name__ == '__main__':
+    main()
--- Medium/combination_sum3.py
+++ Medium/combination_sum3.py
@@ -1,1 +1,85 @@
-__author__ = 'jie'
+# 8/15 - Array, BackTracking
+# Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be
+# used and each combination should be a unique set of numbers.
+# Ensure that numbers within the set are sorted in ascending order.
+#
+# BackTracking is supposed to be depth-first search
+
+import copy
+
+class CombinationSum3:
+    result = []  # static variable to store result
+
+    # @param {integer} k
+    # @param {integer} n
+    # @return {integer[][]}
+
+    # Test on LeetCode - 60ms
+    # Slightly faster than iterative - no need to keep uppers
+    def combination_sum3(self, k, n):
+        CombinationSum3.result = []
+        self.visit(0, k, n, [])
+        return CombinationSum3.result
+
+    def visit(self, i, k, n, combination):
+        if k == 0 and n == 0:
+                CombinationSum3.result.append(copy.deepcopy(combination))
+        elif self.is_promising(i, k, n):
+            i += 1
+            """ frequent use of deepcopy
+            lc = copy.deepcopy(combination)
+            lc.append(i)
+            rc = combination
+            self.visit(i, k-1, n-i, lc)
+            self.visit(i, k, n, rc)"""
+            combination.append(i)
+            self.visit(i, k-1, n-i, combination)
+            combination.pop()  # avoid frequent use of deepcopy
+            self.visit(i, k, n, combination)
+
+    def is_promising(self, i, k, n):
+        return i < 9 and n >= k * (i+1)
+
+    # Test on LeetCode - 100ms (Not Good)
+    # save parent node for the next round analysis
+    def combination_sum3_iterative(self, k, n):
+        CombinationSum3.result = []
+        root = Node([], n)
+        uppers = [root]
+        for i in range(1, 10):
+            lowers = []
+            for node in uppers:
+                if self.is_valid(k, i, node):
+                    nums = copy.deepcopy(node.nums)
+                    nums.append(i)
+                    expand = Node(nums, node.left-i)
+                    if self.is_success(k, expand):
+                        CombinationSum3.result.append(expand.nums)
+                    else:
+                        lowers.append(expand)
+                        lowers.append(node)
+            uppers = lowers
+        return CombinationSum3.result
+
+    def is_valid(self, k, i, node):
+        return node.left >= (k - len(node.nums)) * i
+
+    def is_success(self, k, node):
+        return node.left == 0 and len(node.nums) == k
+
+# each node contains the following information:
+# nums - the numbers included
+# left - difference to make up n
+class Node:
+    def __init__(self, nums, left):
+        self.nums = nums
+        self.left = left
+
+
+def main():
+    test = CombinationSum3()
+    print test.combination_sum3_iterative(3, 15)
+
+
+if __name__ == '__main__':
+    main()
