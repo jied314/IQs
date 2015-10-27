d3f86186841435827de2c25fdfec7cd788313d3b
jied <jied314@mail.sfsu.edu>
2015-10-27 16:02:39 -0700

    10/27 - bst

---
+++ Medium/validate_binary_search_tree.py
@@ -0,0 +1,50 @@
+# 10/27 - Tree, DFS
+# Given a binary tree, determine if it is a valid binary search tree (BST).
+# Assume a BST is defined as follows:
+#   The left subtree of a node contains only nodes with keys less than the node's key.
+#   The right subtree of a node contains only nodes with keys greater than the node's key.
+#   Both the left and right subtrees must also be binary search trees.
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+# def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class ValidateBinarySearchTree(object):
+    # Test on LeetCode - 72ms
+    # Idea:
+    #   left_child < root < right_child && predecessor < root < successor
+    def is_valid_BST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is not None:
+            root_val = root.val
+            if root.left is not None:
+                if root.left.val >= root_val or self.get_left_neighbor(root).val >= root_val or \
+                        not self.is_valid_BST(root.left):
+                    return False
+            if root.right is not None:
+                if root.right.val <= root_val or self.get_right_neighbor(root).val <= root_val or \
+                        not self.is_valid_BST(root.right):
+                        return False
+        return True
+
+    # return the rightmost predecessor (predecessor always exists)
+    def get_left_neighbor(self, node):
+        ret = node.left
+        while ret.right is not None:
+            ret = ret.right
+        return ret
+
+    # return the leftmost successor (successor always exists)
+    def get_right_neighbor(self, node):
+        ret = node.right
+        while ret.left is not None:
+            ret = ret.left
+        return ret
+
