876a36ca3c3998ddbd187616780ba3af365dfdd6
jied <jied314@mail.sfsu.edu>
2015-12-27 10:19:16 -0800

    organize folders

--- 1-Python/Easy/remove_duplicates_from_sorted_array.py
+++ 1-Python/Easy/remove_duplicates_from_sorted_array.py
@@ -21,6 +21,24 @@ class RemoveDuplicatesFromSortedArray:
             j += 1
         return i+1
 
+    # 12/25 - Revisit
+    # Test on LeetCode - 88ms
+    # Two pointers: i -> last visited unique value, j -> traverse the list
+    def remove_duplicates_revisit(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums is None or len(nums) == 0:
+            return 0
+        length = len(nums)
+        i = 0
+        for j in range(1, length):
+            if nums[i] != nums[j]:
+                nums[i+1], nums[j] = nums[j], nums[i+1]
+                i += 1
+        return i+1
+
     # # Test on LeetCode - 104ms
     def remove_duplicates_change_value1(self, nums):
         if nums is None or len(nums) < 2:
